cc44ca4daad7e45105da39df22e63479ee136da3
YooWaan <rterunuma@gmail.com>
2011-06-06 12:53:48 +0900

    add log method to this appliation.

--- SimpleApp/src/app/simple/SimpleApp.java
+++ SimpleApp/src/app/simple/SimpleApp.java
@@ -5,15 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.util.Log;
 
 public class SimpleApp extends Activity
 {
+
+	private final String LOG_TAG = "=== Parent === " + SimpleApp.class.getSimpleName();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		Log.i(LOG_TAG, "onCreate()");
         setContentView(R.layout.main);
 
 		Button btn = (Button)findViewById(R.id.btn_lifecycle);
@@ -34,7 +38,43 @@ public class SimpleApp extends Activity
 				}
 			});
 
-
     }
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.i(LOG_TAG, "onStart()");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.i(LOG_TAG,"onRestart()");
+	}
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.i(LOG_TAG,"onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.i(LOG_TAG,"onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.i(LOG_TAG,"onStop");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.i(LOG_TAG,"onDestroy");
+	}
 
 }
