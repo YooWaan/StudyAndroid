779db4dd15986dff22762ce0cf52a81d66e9881d
YooWaan <rterunuma@gmail.com>
2011-06-06 14:12:11 +0900

    update testcase

--- NoteTest/src/app/note/NoteActivityTest.java
+++ NoteTest/src/app/note/NoteActivityTest.java
@@ -1,6 +1,13 @@
 package app.note;
 
+import android.app.Instrumentation;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Button;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.util.Log;
 
 /**
  * This is a simple framework for a test of an Application.  See
@@ -20,18 +27,72 @@ public class NoteActivityTest extends ActivityInstrumentationTestCase2<NoteActiv
 
 
 	public void testList() {
-		getActivity().runOnUiThread( new Runnable() {
-				@Override
-				public void run() {
-				}
-			});
+		try {
+			ActivityMonitor monitor = new ActivityMonitor(app.note.EditActivity.class.getName(), null, false);
+			Instrumentation inst = getInstrumentation();
+			inst.addMonitor(monitor);
+			NoteActivity note = (NoteActivity)getActivity();
 
-		getInstrumentation().waitForIdleSync();
+			inst.waitForIdleSync();
 
-		int count = getActivity().getListView().getAdapter().getCount();
-		android.util.Log.i("Test", "### count " + count);
+			int startCount = getActivity().getListView().getAdapter().getCount();
+			Log.i("Test", "### count " + startCount);
 
-		assertTrue(count > 0);
+			assertTrue(startCount >= 0);
+
+			final Button btn = (Button)note.findViewById(R.id.btn_add);
+			//TouchUtils.clickView(this ,btn);
+			note.runOnUiThread( new Runnable() {
+					@Override
+						public void run() {
+						btn.performClick();
+					}
+				});
+
+
+			EditActivity edit = (EditActivity)getInstrumentation().waitForMonitorWithTimeout(monitor, 2000); 
+
+			assertNotNull(edit);
+
+			final EditText title = (EditText)edit.findViewById(R.id.title);
+			final EditText body = (EditText)edit.findViewById(R.id.body);
+
+			edit.runOnUiThread( new Runnable() {
+					@Override
+					public void run() {
+						title.setText("test title");
+						body.setText("body text");
+					}
+				});
+
+
+			inst.waitForIdleSync();
+
+			final Button save = (Button)edit.findViewById(R.id.btn_save);
+
+			//TouchUtils.clickView(this ,save);
+			edit.runOnUiThread( new Runnable() {
+					@Override
+					public void run() {
+						save.performClick();
+					}
+				});
+
+			//edit.finish();
+
+			inst.waitForIdleSync();
+			//note.refreshList();
+
+			int insertCount = getActivity().getListView().getAdapter().getCount();
+			Log.i("Test", "### update count " + insertCount);
+
+			assertTrue(insertCount > startCount);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.i("### Test ###", "Err --> " + e);
+			fail();
+		}
 	}
 
 }
